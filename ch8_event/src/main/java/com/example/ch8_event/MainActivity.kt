ee0b468cff650f1683e9c024d7678f2b45aa8e8e
kweonsikyung <ekrk6632@naver.com>
2022-07-21 01:12:08 +0900

    수정_1

--- ch8_event/src/main/java/com/example/ch8_event/MainActivity.kt
+++ ch8_event/src/main/java/com/example/ch8_event/MainActivity.kt
@@ -2,51 +2,76 @@ package com.example.ch8_event
 
 import android.os.Bundle
 import android.os.SystemClock
+import android.util.Log
 import android.view.KeyEvent
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.ch8_event.databinding.ActivityMainBinding
 
+
 class MainActivity : AppCompatActivity() {
-
+    // 뒤로가기 버튼을 누른 시각을 저장하는 속성
     var initTime = 0L
+    // 멈춘 시각을 저장하는 속성
     var pauseTime = 0L
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         binding.startButton.setOnClickListener {
-            binding.chronometer.base = SystemClock.elapsedRealtime()+pauseTime
+            binding.chronometer.base = SystemClock.elapsedRealtime() + pauseTime
             binding.chronometer.start()
-            binding.stopButton.isEnabled=true
-            binding.resetButton.isEnabled=true
-            binding.startButton.isEnabled=false
+            // 버튼 표시 여부 조정
+            binding.stopButton.isEnabled = true
+            binding.resetButton.isEnabled = true
+            binding.startButton.isEnabled = false
+            Toast.makeText(this, "START 버튼을 눌렀네요!!",
+                Toast.LENGTH_SHORT).show()
         }
-
+        binding.startButton.setOnLongClickListener{
+            Toast.makeText(this, "해당기능은 지원하지 않습니다.",
+                Toast.LENGTH_SHORT).show()
+            true
+        }
         binding.stopButton.setOnClickListener {
-            pauseTime=binding.chronometer.base - SystemClock.elapsedRealtime()
+            pauseTime = binding.chronometer.base - SystemClock.elapsedRealtime()
             binding.chronometer.stop()
-            binding.stopButton.isEnabled=false
-            binding.resetButton.isEnabled=true
-            binding.startButton.isEnabled=true
+            binding.stopButton.isEnabled = false
+            binding.resetButton.isEnabled = true
+            binding.startButton.isEnabled = true
+            Toast.makeText(this, "STOP 버튼을 눌렀네요!!",
+                Toast.LENGTH_SHORT).show()
         }
+        binding.stopButton.setOnLongClickListener{
+            Toast.makeText(this, "해당기능은 지원하지 않습니다.",
+                Toast.LENGTH_SHORT).show()
 
+            true
+        }
         binding.resetButton.setOnClickListener {
             pauseTime = 0L
             binding.chronometer.base = SystemClock.elapsedRealtime()
             binding.chronometer.stop()
-            binding.stopButton.isEnabled=false
-            binding.resetButton.isEnabled=false
-            binding.startButton.isEnabled=true
+            binding.stopButton.isEnabled = false
+            binding.resetButton.isEnabled = false
+            binding.startButton.isEnabled = true
+            Toast.makeText(this, "RESET 버튼을 눌렀네요!!",
+                Toast.LENGTH_SHORT).show()
+        }
+        binding.resetButton.setOnLongClickListener{
+            Toast.makeText(this, "해당기능은 지원하지 않습니다.",
+                Toast.LENGTH_SHORT).show()
+            true
         }
     }
-
+    // 뒤로가기 버튼 이벤트 핸들러
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
-        if(keyCode === KeyEvent.KEYCODE_BACK){
-            if(System.currentTimeMillis() - initTime > 3000){
-                Toast.makeText(this, "종료하려면 한 번 더 누르세요!!", Toast.LENGTH_SHORT).show()
+        // 뒤로가기 버튼을 눌렀을 때 처리
+        if (keyCode === KeyEvent.KEYCODE_BACK) {
+            // 뒤로가기 버튼을 처음 눌렀거나 누른 지 3초가 지났을 때 처리
+            if (System.currentTimeMillis() - initTime > 3000) {
+                Toast.makeText(this, "종료하려면 한 번 더 누르세요!!",
+                    Toast.LENGTH_SHORT).show()
                 initTime = System.currentTimeMillis()
                 return true
             }
